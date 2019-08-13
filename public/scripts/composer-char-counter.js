1c5c7800f956108de6d5d59c4f54b1ac3379f831
Devon <devon.blake@live.com>
2019-08-13 23:01:53 +0000

    character counter implemented and added to index

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $('.new-tweet form').on('keydown', 'textarea', function(event) {
+
+  const count = $(this).siblings('.counter');
+
+  const counter = 140 - $(this).val().length;
+
+  count.html(counter);
+    if(counter >= 0) {
+      $(count).css('color', 'black');
+       } else {
+         $(count).css('color', 'red');
+      }
+  })
+});
